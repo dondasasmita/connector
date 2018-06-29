3b69ecb80821f04529368ca7f4f1de82410ca8ae
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-06-29 08:13:32 +0700

    established connection to mongoDB

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require ('express')
+const mongoose = require('mongoose')
+
 const app = express()
 
+//DB config
+const db = require('./config/keys').mongoURI
+
+//connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('DB connected'))
+    .catch( err => console.log(err))
+
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.send('Hello!, this is gonna be a great app')
 })
 
 const port = process.env.PORT || 5000
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
-})
+}) 
