05d43f78877e747d4743095d114d9172e5c00ce3
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-06-30 00:17:00 +0700

    added various validations for login and registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport')
 
 //Load input validation
 const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
 
 // Load user model
 const User = require('../models/User')
@@ -33,7 +34,8 @@ router.post('/register', (req,res) => {
     User.findOne({email: req.body.email})
     .then(user => {
         if(user) {
-            return res.status(400).json({email: 'Email already exists'})
+            errors.email = 'Email already exists'
+            return res.status(400).json(errors)
         } else {
             // generate Gravatar URLs 
             const avatar = gravatar.url(req.body.email, {
@@ -66,6 +68,13 @@ router.post('/register', (req,res) => {
 // @desc    Login User / Returning JWT token
 // @access  Public 
 router.post('/login', (req, res) => {
+    const { errors, isValid} = validateLoginInput(req.body)
+
+    //check validation
+    if (!isValid) {
+        return res.status(400).json(errors)
+    }
+
     const email = req.body.email
     const password = req.body.password
 
@@ -74,7 +83,8 @@ router.post('/login', (req, res) => {
     .then(user => {
         //check for user
         if (!user) {
-            return res.status(404).json({email: 'User not found'})
+            errors.email = 'User not found'
+            return res.status(404).json(errors)
         }
         //check password
         bcrypt.compare(password, user.password)
@@ -102,7 +112,8 @@ router.post('/login', (req, res) => {
                 }) 
 
             } else {
-                return res.status(400).json({password: 'Password incorrect'})
+                errors.password = 'Incorrect password'
+                return res.status(400).json(errors)
             }
         })
     })
--- validation/register.js
+++ validation/register.js
@@ -4,8 +4,44 @@ const isEmpty = require('./is-empty')
 module.exports = function validateRegisterInput(data) {
     let errors = {}
 
+    data.username = !isEmpty(data.username) ? data.username : ''
+    data.email = !isEmpty(data.email) ? data.email : ''
+    data.password = !isEmpty(data.password) ? data.password : ''
+    data.password2 = !isEmpty(data.password2) ? data.password2 : ''
+
+    // Validate username length
     if (!Validator.isLength(data.username, {min: 2, max: 30})) {
-        errors.name = 'Username must be between 2 and 30 characters'
+        errors.username = 'Username must be between 2 and 30 characters'
+    }
+
+    if (Validator.isEmpty(data.username)) {
+        errors.username = 'Username is required'
+    }
+
+    // Validate email address
+    if (Validator.isEmpty(data.email)) {
+        errors.email = 'Email is required'
+    }
+
+    if (!Validator.isEmail(data.email)) {
+        errors.email = 'Enter a valid email'
+    }
+
+    // Validate password
+    if (Validator.isEmpty(data.password)) {
+        errors.password = 'Password is required'
+    }
+
+    if (!Validator.isLength(data.password, {min: 5 , max: 30})) {
+        errors.password = 'Password must be between 5 to 30 characters'
+    }
+
+    if (Validator.isEmpty(data.password2)) {
+        errors.password2 = 'Confirm your password'
+    }
+
+    if (!Validator.equals(data.password, data.password2)) {
+        errors.password2 = 'Passwords must match'
     }
 
     return {
