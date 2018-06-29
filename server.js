737129045c93fa8e837ec5a7e55fa886cfa4b379
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-06-29 10:37:19 +0700

    load body parser dependency

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require ('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
 
 const app = express()
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 
 //DB config
 const db = require('./config/keys').mongoURI
@@ -15,6 +25,11 @@ mongoose
 app.get('/', (req, res) => {
     res.send('Hello!, this is gonna be a great app')
 })
+
+// Use Routes
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
 const port = process.env.PORT || 5000
 
