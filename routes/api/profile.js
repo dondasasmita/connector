2405abe55e4aece89f5294f4f102a05ea8dd3531
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-07-02 11:21:36 +0700

    added route for deleting user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -204,6 +204,22 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
 })
 
 /*
+@route   DELETE api/profile
+@desc    Delete user and profile
+@access  Private
+*/
+router.delete('/', passport.authenticate('jwt', {session: false}),(req,res) => {
+    Profile.findOneAndRemove({user: req.user.id})
+    .then( () => {
+        User.findOneAndRemove({_id: req.user.id})
+        .then(() => res.status(200).json({success: 'User has been removed'}) )
+    })
+    .catch(err => {
+        res.status(404).json(err)
+    })
+})
+
+/*
 @route   POST api/profile/experience
 @desc    Add experience to profile
 @access  Private
